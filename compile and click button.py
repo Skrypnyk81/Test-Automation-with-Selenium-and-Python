c89ce7d7323bb69201b034aae70efac55b7cd7ae
Yuriy Skrypnyk <skrypnyk@hotmail.it>
2019-09-03 12:22:47 +0200

    Update compile and click button.py

--- compile and click button.py
+++ compile and click button.py
@@ -1,18 +1,27 @@
 from selenium import webdriver
+import time
+
+link = "http://suninjuly.github.io/find_xpath_form"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element_by_tag_name("input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_name("last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_class_name("form-control.city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element_by_id("country")
+    input4.send_keys("Russia")
+    button = browser.find_element_by_xpath('//button[text()="Submit"]')
+    button.click()
 
 
-browser = webdriver.Chrome()
-browser.get("http://suninjuly.github.io/find_xpath_form")
-
-input1 = browser.find_element_by_tag_name("input")
-input1.send_keys("Ivan")
-input2 = browser.find_element_by_name("last_name")
-input2.send_keys("Petrov")
-input3 = browser.find_element_by_class_name("form-control.city")
-input3.send_keys("Smolensk")
-input4 = browser.find_element_by_id("country")
-input4.send_keys("Russia")
-button = browser.find_element_by_xpath('//button[text()="Отправить"]')
-button.click()
-
-
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
